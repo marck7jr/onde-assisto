1f47b525d434ce5cb21385255e2d19dae8917ad7
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2020-01-08 04:37:11 +0000

    Merged PR 11: Adiciona Página Feedback
    Adiciona Página Feedback.
    
    Related work items: #131, #133, #134, #141

--- OndeAssisto.Web.App/Startup.cs
+++ OndeAssisto.Web.App/Startup.cs
@@ -19,6 +19,10 @@ namespace OndeAssisto.Web.App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLocalization(options => 
+            {
+                options.ResourcesPath = "Resources";
+            });
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddApplicationInsightsTelemetry();
